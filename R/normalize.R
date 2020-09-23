e4e00c4844b68d8ee72ae2e5ab5177da64d3e8f3
eyackulic <62761629+eyackulic@users.noreply.github.com>
2020-09-23 14:53:49 -0700

    normalize.R
    updated functions so that normalize function works with nested functions; standardize needed an update that I made previously

--- R/normalize.R
+++ R/normalize.R
@@ -16,18 +16,11 @@
 #'
 filechooseR <- function(id,
                         directory = NA,
-                      spectra,
-                      data.file = NA,
-                      white.ref.file = NA,
-                      dark.ref.file = NA){
+                      spectra){
     Filters <- matrix(c("*",".raw"),1, 2, byrow = TRUE)
 data <- file.path(directory,paste(id,".raw",sep=""))
   if(missing(data)) data <- tcltk::tk_choose.files(caption="choose Data File",filter = Filters)
   filen <- raster::brick(data)
-
-dark <- file.path(directory,paste("DARKREF_",id,".raw",sep=""))
-  if(missing(dark))  dark <-tcltk::tk_choose.files(caption="choose 'DARKREF' File",filter = Filters)
-
   raw <- raster::subset(filen,spectra)
 return(raw)
 }
@@ -44,28 +37,30 @@ return(stripe)
 WhiteRef <-function(stripe,directory,id){
   white <- file.path(directory,paste("WHITEREF_",id,".raw",sep=""))
   if(missing(white))  white <- tcltk::tk_choose.files(caption="choose 'WHITEREF' File",filter = Filters)
-    whiteRow <- createReferenceMeanRow(white,stripe,"WhiteRow.tif",spectra)
+    whiteRow <- createReferenceMeanRow(refFile = white,e = stripe,outFile="WhiteRow.tif",spectra=spectra)
   names(whiteRow) <- names(stripe)
   #find length of core stripe to break up white
   len <- stripe@nrows
   #disaggregate whiterow to length
-  whiteRef <- disaggregate(whiteRow,fact = c(1,len))
+  white.ref <- disaggregate(whiteRow,fact = c(1,len))
   #set extents to be the same between stripe and reference files
-  extent(whiteRef) <- extent(stripe)
-  return(whiteRef)
+  extent(white.ref) <- extent(stripe)
+  return(white.ref)
 }
 
-DarkRef <- function(stripe,dark){
-  darkRow <- createReferenceMeanRow(dark,stripe,"DarkRow.tif",spectra)
+DarkRef <- function(stripe,directory,id){
+  dark <- file.path(directory,paste("DARKREF_",id,".raw",sep=""))
+  if(missing(dark))  dark <- tcltk::tk_choose.files(caption="choose 'DARKREF' File",filter = Filters)
+  darkRow <- createReferenceMeanRow(refFile=dark,e=stripe,outFile="DarkRow.tif",spectra=spectra)
   names(darkRow) <- names(stripe)
   len <- stripe@nrows
-  darkRef <- disaggregate(darkRow,fact = c(1,len))
-  extent(darkRef) <- extent(stripe)
-return(darkRef)
+  dark.ref <- disaggregate(darkRow,fact = c(1,len))
+  extent(dark.ref) <- extent(stripe)
+return(dark.ref)
 }
 
-overlayR <- function(stripe,whiteRef,darkRef){
-  normalized <- raster::overlay(stripe,whiteRef,darkRef,fun = normFun,filename = "normalized.tif",overwrite = TRUE)
+overlayR <- function(stripe,white.ref,dark.ref){
+  normalized <- raster::overlay(stripe,white.ref,dark.ref,fun = normFun,filename = "normalized.tif",overwrite = TRUE)
   names(normalized) <- names(stripe)
   return(normalized)
 }
@@ -78,14 +73,14 @@ normalize <- function(id,spectra,directory,tif.path.to.write = NA){
                      directory = directory)
 
   #crop the image
-  stripe <- cropImage(raw)
+  stripe <- cropImage(raw = raw)
 
   #load in the white and dark refs
-  white <- whiteRef(stripe,id,directory)
-  dark <- darkRef(stripe,id,directory)
+  white.ref <- WhiteRef(stripe = stripe,id = id,directory = directory)
+  dark.ref <- DarkRef(stripe = stripe,id = id,directory = directory)
 
   #now normalize
-  normalized <- overlayR(stripe, white, dark)
+  normalized <- overlayR(stripe = stripe, white.ref = white.ref, dark.ref = dark.ref)
 
   #optionally write to a tif
   if(!is.na(tif.path.to.write)){
--- Untitled.R
+++ Untitled.R
@@ -14,8 +14,17 @@ library(progress)
 source('~/Desktop/INF_550_Notes/standardize.R')
 
 #go to directory where raw files are saved
-(directory <- setwd("/Volumes/easystore/Forsy"))
+
 
 #choose band_names
 spectra <- c("X569.71","X589.88","X615.22","X630.49","X659.89","X665.02","X689.43","X730.74","X790.43","X845.12","X899.86")
 id <- "Lakes380_FORSY_LC1U_2B_test_2020-06-05_04-05-39"
+(directory <- setwd("/Volumes/easystore/Forsy"))
+core <- normalize(directory=directory,id=id,spectra=spectra)
+
+raw <- filechooseR(id=id,directory=directory,spectra=spectra)
+stripe <- cropImage(raw)
+whiteRef <-WhiteRef(stripe=stripe,directory = directory,id=id)
+darkRef <-DarkRef(stripe,directory=directory,id=id)
+normalized <- overlayR(stripe,whiteRef,darkRef)
+
