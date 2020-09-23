4e4f5dd044575d8a71564004545bb5165f434ad3
nickmckay <nicholas.mckay@nau.edu>
2020-09-23 10:32:15 -0400

    shuffling and shiny roi

---
+++ R/normalize.R
@@ -0,0 +1,82 @@
+#load libraries
+library(raster)
+library(tictoc)
+#library(devtools)
+#devtools::install_github("timelyportfolio/svgPanZoom")
+#devtools::install_github("duncantl/SVGAnnotation")
+library(svglite)
+library(svgPanZoom)
+library(SVGAnnotation)
+library(tcltk)
+library(progress)
+
+#internal functions
+source('~/Desktop/INF_550_Notes/standardize.R')
+
+#go to directory where raw files are saved
+setwd()
+
+#choose band_names
+spectra <- c("X569.71","X589.88","X615.22","X630.49","X659.89","X665.02","X689.43","X730.74","X790.43","X845.12","X899.86")
+
+#function
+#' Title
+#'
+#' @param spectra
+#' @param data.file
+#' @param white.ref.file
+#' @param dark.ref.file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+normalize <- function(spectra,
+                       data.file = NA,
+                       white.ref.file = NA,
+                       dark.ref.file = NA){
+
+    Filters <- matrix(c("*",".raw"),1, 2, byrow = TRUE)
+
+  data <- tcltk::tk_choose.files(caption="choose Data File",filter = Filters)
+  filen <- raster::brick(data)
+
+  white <- tk_choose.files(caption="choose 'WHITEREF' File",filter = Filters)
+  dark <-tk_choose.files(caption="choose 'DARKREF' File",filter = Filters)
+
+  raw <- raster::subset(filen,spectra)
+
+
+
+  #is there an option to add extra zoom?
+  image <- raster::subset(raw,c(1,5,8))
+  raster::plotRGB(raw,c(1,5,8), axes=TRUE, stretch="hist", main="Raw Image")
+  cropC <- raster::drawExtent()
+  stripe <- crop(raw,cropC)
+  names(stripe) <- names(raw)
+
+  #header file has to exist in same file or this breaks!
+  whiteRow <- createReferenceMeanRow(white,stripe,"WhiteRow.tif",names)
+  names(whiteRow) <- names(stripe)
+  darkRow <- createReferenceMeanRow(dark,stripe,"DarkRow.tif",names)
+  names(darkRow) <- names(stripe)
+
+  #find length of core stripe to break up white
+  len <- stripe@nrows
+
+  #disaggregate whiterow to length
+  whiteRef <- disaggregate(whiteRow,fact = c(1,len))
+  darkRef <- disaggregate(darkRow,fact = c(1,len))
+
+  #set extents to be the same between stripe and reference files
+  extent(whiteRef) <- extent(stripe)
+  extent(darkRef) <- extent(stripe)
+
+  #normalization is fast! 47 seconds for Tutae
+  normalized <- overlay(stripe,whiteRef,darkRef,fun = normFun,filename = "normalized.tif",overwrite = TRUE)
+  names(normalized) <- names(stripe)
+
+  return(normalized)
+}
+
+core_name <- normalizeR()
---
+++ R/roi.R
@@ -0,0 +1,159 @@
+pick_roi_simple <- function(image){
+  raster::plotRGB(image, axes=TRUE, stretch="hist", main="Raw Image")
+  cropC <- raster::drawExtent()
+  return(cropC)
+}
+
+
+
+cropViewFun <- function(click,dblclick){
+  xmin <- min(click$x,dblclick$x)
+  xmax <- max(click$x,dblclick$x)
+  ymin <- min(click$y,dblclick$y)
+  ymax <- max(click$y,dblclick$y)
+
+  xy <- data.frame(x = c(xmin,xmax,xmax,xmin),y = c(ymin,ymin,ymax,ymax))
+  polygon(xy,col=NA,border = "red")
+  clickpoints <- rbind(c(click$x,click$y),c(dblclick$x,dblclick$y))
+  points(clickpoints,cex = 2)
+  points(clickpoints, pch=3, col='green')
+
+
+}
+
+server <- function(input, output) {
+
+  brushExtent <- reactiveValues()
+  click <- reactiveValues()
+  dblclick <- reactiveValues()
+
+  click$x <- 0
+  click$y <- 0
+  dblclick$x <- 0
+  dblclick$y <- 0
+
+
+  brushExtent$extent <- raster::extent(image)
+
+  output$plot1 <- renderPlot({
+    raster::plotRGB(image, axes=TRUE, stretch="hist", main="Overview",addfun = cropViewFun(click,dblclick))
+  })
+
+  output$plot2 <- renderPlot({
+    raster::plotRGB(image, axes=TRUE, stretch="hist", main="Zoom Control")
+  })
+
+  output$plot3 <- renderPlot({
+    raster::plotRGB(image, axes=TRUE, stretch="hist", main="Select Crop Corners",ext = brushExtent$extent)
+  })
+
+  # If so, zoom to the brush bounds; if not, reset the zoom.
+  observe({
+    brush <- input$plot2_brush
+    if (!is.null(brush)) {
+      brushExtent$extent <- raster::extent(brush$xmin, brush$xmax,brush$ymin, brush$ymax)
+
+    } else {
+      brushExtent$extent <- raster::extent(image)
+    }
+
+    brushExtent$brushBox <- data.frame(x = c(brush$xmin, brush$xmax,brush$xmax, brush$xmin),
+                           y = c(brush$ymin, brush$ymin,brush$ymax, brush$ymax))
+
+
+
+
+  })
+
+  observeEvent(input$image_click,{
+    click$x <- input$image_click$x
+    click$y <- input$image_click$y
+  }
+  )
+
+  observeEvent(input$image_dblclick,{
+    dblclick$x <- input$image_dblclick$x
+    dblclick$y <- input$image_dblclick$y
+  }
+  )
+
+  output$click_info <- renderPrint({
+    cat("click:\n")
+    str(c(click$x,click$y))
+  })
+
+  output$dblclick_info <- renderPrint({
+    cat("dblclick:\n")
+    str(c(dblclick$x,dblclick$y))
+  })
+
+  observeEvent(input$record, {
+
+    xmin <- min(click$x,dblclick$x)
+    xmax <- max(click$x,dblclick$x)
+    ymin <- min(click$y,dblclick$y)
+    ymax <- max(click$y,dblclick$y)
+    crp <- raster::extent(c(xmin,xmax,ymin,ymax))
+    crpout <<- crp
+    shiny::stopApp()
+
+
+  })
+
+}
+pick_roi_shiny <- function(image){
+
+  ui <- fluidPage(
+    # Some custom CSS for a smaller font for preformatted text
+    tags$head(
+      tags$style(HTML("
+      pre, table.table {
+        font-size: smaller;
+      }
+    "))
+    ),
+
+    column(width = 12, class = "well",
+           h2("Region of interest (ROI) selector"),
+           fluidRow(
+             column(width = 4,
+                    plotOutput("plot1", height = 600)
+             ),
+             column(width = 4,
+                    plotOutput("plot2", height = 600,
+                               brush = brushOpts(
+                                 id = "plot2_brush",
+                                 resetOnNew = TRUE
+                               )
+                    )
+             ),
+             column(width = 4,
+                    plotOutput("plot3",
+                               height = 600,
+                               click = "image_click",
+                               dblclick = dblclickOpts(
+                                 id = "image_dblclick"
+                               )
+                    )
+             )
+           ),
+           fluidRow(
+             column(width = 6,
+                    verbatimTextOutput("click_info")
+             ),
+             column(width = 6,
+                    verbatimTextOutput("dblclick_info")
+             )
+           )
+
+
+    ),
+    actionButton("record", "Record ROI")
+  )
+
+
+
+ts <- shinyApp(ui, server)
+runApp(ts)
+return(crpout)
+}
