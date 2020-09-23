d15429c98e70d6205bce8049b7040e49e88a3f73
eyackulic <62761629+eyackulic@users.noreply.github.com>
2020-09-23 12:41:39 -0700

    created overall function

--- R/normalize.R
+++ R/normalize.R
@@ -1,23 +1,3 @@
-#load libraries
-library(raster)
-library(tictoc)
-#library(devtools)
-#devtools::install_github("timelyportfolio/svgPanZoom")
-#devtools::install_github("duncantl/SVGAnnotation")
-library(svglite)
-library(svgPanZoom)
-library(SVGAnnotation)
-library(tcltk)
-library(progress)
-
-#internal functions
-source('~/Desktop/INF_550_Notes/standardize.R')
-
-#go to directory where raw files are saved
-(directory <- setwd("/Volumes/easystore/Forsy"))
-
-#choose band_names
-spectra <- c("X569.71","X589.88","X615.22","X630.49","X659.89","X665.02","X689.43","X730.74","X790.43","X845.12","X899.86")
 
 #function
 #' Title
@@ -32,9 +12,10 @@ spectra <- c("X569.71","X589.88","X615.22","X630.49","X659.89","X665.02","X689.4
 #'
 #' @examples
 
-id <- "Lakes380_FORSY_LC1U_2B_test_2020-06-05_04-05-39"
 
-normalize <- function(id,
+
+filechooseR <- function(id,
+                        directory = NA,
                       spectra,
                       data.file = NA,
                       white.ref.file = NA,
@@ -43,8 +24,7 @@ normalize <- function(id,
 data <- file.path(directory,paste(id,".raw",sep=""))
   if(missing(data)) data <- tcltk::tk_choose.files(caption="choose Data File",filter = Filters)
   filen <- raster::brick(data)
-white <- file.path(directory,paste("WHITEREF_",id,".raw",sep=""))
-  if(missing(white))  white <- tcltk::tk_choose.files(caption="choose 'WHITEREF' File",filter = Filters)
+
 dark <- file.path(directory,paste("DARKREF_",id,".raw",sep=""))
   if(missing(dark))  dark <-tcltk::tk_choose.files(caption="choose 'DARKREF' File",filter = Filters)
 
@@ -61,7 +41,9 @@ cropImage <- function(raw){
 return(stripe)
 }
 
-WhiteRef <-function(stripe,white){
+WhiteRef <-function(stripe,directory,id){
+  white <- file.path(directory,paste("WHITEREF_",id,".raw",sep=""))
+  if(missing(white))  white <- tcltk::tk_choose.files(caption="choose 'WHITEREF' File",filter = Filters)
     whiteRow <- createReferenceMeanRow(white,stripe,"WhiteRow.tif",spectra)
   names(whiteRow) <- names(stripe)
   #find length of core stripe to break up white
@@ -72,7 +54,7 @@ WhiteRef <-function(stripe,white){
   extent(whiteRef) <- extent(stripe)
   return(whiteRef)
 }
-  
+
 DarkRef <- function(stripe,dark){
   darkRow <- createReferenceMeanRow(dark,stripe,"DarkRow.tif",spectra)
   names(darkRow) <- names(stripe)
@@ -88,5 +70,27 @@ overlayR <- function(stripe,whiteRef,darkRef){
   return(normalized)
 }
 
-core_name <- normalize(spectra = spectra)
-core_name
+
+normalize <- function(id,spectra,directory,tif.path.to.write = NA){
+  #choose and load the file
+  raw <- filechooseR(id = id,
+                     spectra = spectra,
+                     directory = directory)
+
+  #crop the image
+  stripe <- cropImage(raw)
+
+  #load in the white and dark refs
+  white <- whiteRef(stripe,id,directory)
+  dark <- darkRef(stripe,id,directory)
+
+  #now normalize
+  normalized <- overlayR(stripe, white, dark)
+
+  #optionally write to a tif
+  if(!is.na(tif.path.to.write)){
+    writeTif(normalized, path = tif.path.to.write)
+  }
+
+  return(normalized)
+}
