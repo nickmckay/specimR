c1b27e91cc9b33df2c2f5120754ce29720631fcd
mzarowka <maurycy.zarczynski@outlook.com>
2023-01-24 16:04:58 -0700

    Add option to write composite drive data to file

--- R/composite_drive_depth_mz.R
+++ R/composite_drive_depth_mz.R
@@ -1,13 +1,16 @@
 #' Create composite proxy profile from multiple ROIs
 #'
 #' @param directory character path to selected drive
+#' @param .tofile if TRUE write output to file
 #'
 #' @return a tibble with composite record for selected drive
 #' @export
 #'
+#' @description If there are multiple ROIs within single drive, merge them into composite
+#' Works with purrr::map for multiple drives if directory is a vector of paths
+#'
 #' @examples
-create_composite_drive <- function(directory = NA_character_) {
-
+create_composite_drive <- function(directory = NA_character_, .tofile = TRUE) {
   # List depth table files in the root drive directory
   depth_data <- fs::dir_ls(directory, regexp = "depthTable", recurse = TRUE) |>
     # Filter so paths to photos are not recorded
@@ -36,6 +39,11 @@ create_composite_drive <- function(directory = NA_character_) {
     # Composite depth
     dplyr::mutate(composite = depth + cm, .after = depth)
 
-  # Return data
-  return(translated_data)
+  # Should tibble be written to file
+  if (.tofile == TRUE) {
+    readr::write_csv(x = translated_data, file = paste0(directory, "/composite.csv"))
+  } else {
+    # Return data
+    return(translated_data)
+  }
 }
