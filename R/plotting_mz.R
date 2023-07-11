c89c0c7ecd9dd8514e0e8aaf61f7ac64403f2281
mzarowka <maurycy.zarczynski@outlook.com>
2023-07-10 19:09:59 -0700

    Templates for general workflow and plotting

---
+++ R/plotting_mz.R
@@ -0,0 +1,5 @@
+# Plotting
+# Plotting RGB file
+# Plotting colored map of proxy (RABD, Ratio etc)
+# Plotting profile
+# Put core and profile/map pairs along - let user choose
---
+++ R/workflow_mz.R
@@ -0,0 +1,10 @@
+# General workflow
+# Get capture raster from shiny output
+# Get white reference raster from shiny output
+# Get dark reference raster from shiny output
+# Set workflow mode 1: normalize or not
+# Set workflow mode 2: normalize full or cropped
+# Set workflow mode 3: normalize rois only?
+# Set workflow mode 4: normalize subset of layers
+# Set workflow mode 5: different white reference capture time
+#
